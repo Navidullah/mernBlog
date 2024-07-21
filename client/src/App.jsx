da918ec4a409af88dddedf96b55fd5da3cd6709d
komal ,k c~x <komaalfarid@gmail.com>
2024-07-21 07:09:51 +0500

    Create Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Projects from "./pages/Projects.jsx";
+
 function App() {
   return (
-    <>
-      <div className="text-3xl text-red-400">welcome to the pulse</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>sign in page</div>;
+};
+
+export default SignIn;
